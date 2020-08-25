e0cf3b7d5017c5c732fae4a30a160d9572776646
harunavictor <44639372+harunavictor@users.noreply.github.com>
2020-08-25 20:37:34 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -51,7 +51,7 @@ export class AppComponent implements OnInit {
       },
       { validator: PasswordValidation }
     );
-
+    
     this.registrationForm
       .get('subscribe')
       .valueChanges.subscribe((checkedValues) => {
